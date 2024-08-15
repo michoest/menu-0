51724ab20aff66db125234cb978eeb7719ae097f
Michael Oesterle <michaeloesterle01@gmail.com>
2024-08-15 12:37:57 +0200

    Add completeItem route

--- api/api.js
+++ api/api.js
@@ -10,42 +10,61 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Sample data
-const users = [
-  { id: 1, name: 'Roßes' },
-  { id: 2, name: 'Leines' },
-  { id: 3, name: 'Bychen' }
-];
+const db = {
+    menu: [
+        { id: 1, title: 'Waktoffels', standardAmount: 4, ingredients: [
+          { name: 'Toffels', amount: { value: 1, unit: 'kg' } }, 
+          { name: 'Wak', amount: { value: 250, unit: 'g' } }
+          ] 
+      },
+        { id: 2, title: 'Sagne', standardAmount: 2, ingredients: [
+          { name: 'Dels', amount: { value: 12, unit: 'Latten' } }, 
+          { name: 'Sauce', amount: { value: null, unit: 'viel' } }
+      ] }
+      ],
+      list: [
+        { id: 1, category: 'We', name: 'Eis in Röße M', notes: '6 Tück', status: 'open', due: '2024-08-16' },
+        { id: 2, category: 'We', name: 'Lopier', notes: '', status: 'open' },
+        { id: 3, category: 'Markt', name: 'Toffels', notes: '2,5 kg', status: 'open' }
+      ]
+};
 
-const menu = [
-  { id: 1, title: 'Waktoffels', standardAmount: 4, ingredients: [
-    { name: 'Toffels', amount: { value: 1, unit: 'kg' } }, 
-    { name: 'Wak', amount: { value: 250, unit: 'g' } }
-    ] 
-},
-  { id: 2, title: 'Sagne', standardAmount: 2, ingredients: [
-    { name: 'Dels', amount: { value: 12, unit: 'Latten' } }, 
-    { name: 'Sauce', amount: { value: null, unit: 'viel' } }
-] }
-];
+// const users = [
+//   { id: 1, name: 'Roßes' },
+//   { id: 2, name: 'Leines' },
+//   { id: 3, name: 'Bychen' }
+// ];
 
-const list = [
-  { id: 1, category: 'We', name: 'Eis in Röße M', notes: '6 Tück', status: 'open', due: '2024-08-16' },
-  { id: 2, category: 'We', name: 'Lopier', notes: '', status: 'open' },
-  { id: 3, category: 'Markt', name: 'Toffels', notes: '2,5 kg', status: 'open' }
-];
+// const menu = [
+//   { id: 1, title: 'Waktoffels', standardAmount: 4, ingredients: [
+//     { name: 'Toffels', amount: { value: 1, unit: 'kg' } }, 
+//     { name: 'Wak', amount: { value: 250, unit: 'g' } }
+//     ] 
+// },
+//   { id: 2, title: 'Sagne', standardAmount: 2, ingredients: [
+//     { name: 'Dels', amount: { value: 12, unit: 'Latten' } }, 
+//     { name: 'Sauce', amount: { value: null, unit: 'viel' } }
+// ] }
+// ];
+
+// const list = [
+//   { id: 1, category: 'We', name: 'Eis in Röße M', notes: '6 Tück', status: 'open', due: '2024-08-16' },
+//   { id: 2, category: 'We', name: 'Lopier', notes: '', status: 'open' },
+//   { id: 3, category: 'Markt', name: 'Toffels', notes: '2,5 kg', status: 'open' }
+// ];
 
 // Routes
-app.get('/users', async (req, res) => {
-  try {
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching users' });
-  }
-});
+// app.get('/users', async (req, res) => {
+//   try {
+//     res.json(users);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error fetching users' });
+//   }
+// });
 
 app.get('/menu', async (req, res) => {
   try {
-    res.json(menu);
+    res.json(db.menu);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching menu' });
   }
@@ -53,10 +72,18 @@ app.get('/menu', async (req, res) => {
 
 app.get('/list', async (req, res) => {
   try {
-    res.json(list);
+    res.json(db.list);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching list' });
   }
+});
+
+router.post('/item/:id/complete', async (req, res, next) => {
+    const item = db.list.find(item => item.id == req.params.id);
+
+    item.status = 'completed';
+    
+    return res.json(db.list);
 });
 
 // Start the server

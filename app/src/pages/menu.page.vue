4c68efd8dd4c9802761fc4a8bd5ce37fe270c5dc
Michael Oesterle <michaeloesterle01@gmail.com>
2024-09-14 21:40:54 +0200

    Lint

--- app/src/pages/menu.page.vue
+++ app/src/pages/menu.page.vue
@@ -15,56 +15,61 @@
   <q-page v-else>
     <q-list v-if="dishes.length > 0" class="q-pt-md">
       <q-item class="flex justify-center">
-                <q-input v-model="search" standout rounded dense clearable placeholder="Mmmmhhhes Schompf" style="width: 60%; min-width: 300px;">
-                    <template v-slot:prepend>
-                        <q-icon name="search" />
-                    </template>
-                </q-input>
-            </q-item>
+        <q-input v-model="search" standout rounded dense clearable placeholder="Mmmmhhhes Schompf"
+          style="width: 60%; min-width: 300px;">
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </q-item>
 
-            <q-expansion-item expand-icon-toggle switch-toggle-side
-                v-for="dish in filteredDishes" :key="dish.dish.id" :content-inset-level="1">
-                    <template v-slot:header>
-                        <q-item-section>
-                            <q-item-label>{{ dish.dish.title }}</q-item-label>
-                            <q-item-label caption>Übliche Menge: {{ dish.dish.standardAmount }}</q-item-label>
-                        </q-item-section>
-                        <q-item-section side>
-                            <div class="text-grey-8 q-gutter-xs flex items-center">
-                                <q-btn color="red" :disable="dish.amount == 0" flat round icon="remove"
-                                    @click="onClickDecrementDish(dish)" />
-                                <span>{{ dish.amount }}</span>
-                                <q-btn color="green" flat round icon="add" @click="onClickIncrementDish(dish)" />
-                            </div>
-                        </q-item-section>
-                    </template>
-                    <q-item>
-                      <q-item-section>
-                        <div>
-                          <span class="text-bold">Zutaten pro Portion:</span> {{ dish.dish.ingredients.map(ingredient => ingredientToString(ingredient)).join(', ') }}
-                        </div>
-                        <div v-if="dish.dish.subdishes && dish.dish.subdishes.length > 0" class="q-mt-md">
-                          <span class="text-bold">Sub-dishes:</span> {{ dish.dish.subdishes.map(subdish => `${dishes.find(dish => dish.dish.id == subdish.dish).dish.title} (Menge: ${subdish.amountFactor})`).join(', ') }}
-                        </div>
-                        </q-item-section>
-                      <q-item-section side>
-                        <q-btn flat round icon="edit" @click="onClickEditDish(dish.dish)" />
-                      </q-item-section>
-                    </q-item>
-                    <q-separator inset />
+      <q-expansion-item expand-icon-toggle switch-toggle-side v-for="dish in filteredDishes" :key="dish.dish.id"
+        :content-inset-level="1">
+        <template v-slot:header>
+          <q-item-section>
+            <q-item-label>{{ dish.dish.title }}</q-item-label>
+            <q-item-label caption>Übliche Menge: {{ dish.dish.standardAmount }}</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <div class="text-grey-8 q-gutter-xs flex items-center">
+              <q-btn color="red" :disable="dish.amount == 0" flat round icon="remove"
+                @click="onClickDecrementDish(dish)" />
+              <span>{{ dish.amount }}</span>
+              <q-btn color="green" flat round icon="add" @click="onClickIncrementDish(dish)" />
+            </div>
+          </q-item-section>
+        </template>
+        <q-item>
+          <q-item-section>
+            <div>
+              <span class="text-bold">Zutaten pro Portion:</span> {{ dish.dish.ingredients.map(ingredient =>
+                ingredientToString(ingredient)).join(', ') }}
+            </div>
+            <div v-if="dish.dish.subdishes && dish.dish.subdishes.length > 0" class="q-mt-md">
+              <span class="text-bold">Sub-dishes:</span> {{ dish.dish.subdishes.map(subdish => `${dishes.find(dish =>
+                dish.dish.id == subdish.dish).dish.title} (Menge: ${subdish.amountFactor})`).join(', ') }}
+            </div>
+            <div v-if="dish.dish.recipe" class="q-mt-md">
+              <span class="text-bold">Recipe:</span> <q-btn type="a" :href="dish.dish.recipe" target="_blank"
+                :label="dish.dish.title" />
+            </div>
+          </q-item-section>
+          <q-item-section side>
+            <q-btn flat round icon="edit" @click="onClickEditDish(dish.dish)" />
+          </q-item-section>
+        </q-item>
+        <q-separator inset />
 
-                </q-expansion-item>
+      </q-expansion-item>
     </q-list>
 
     <q-page-sticky position="bottom" :offset="[0, -28]" style="z-index: 6000;">
-      <q-btn fab icon="checklist" color="accent" @click="onClickChooseIngredients" :disabled="disableChooseIngredients" />
+      <q-btn fab icon="checklist" color="accent" @click="onClickChooseIngredients"
+        :disabled="disableChooseIngredients" />
     </q-page-sticky>
 
-    <ChooseIngredientsDialog
-        v-model:model-value="chooseIngredientsDialog.show"
-        :dishes="chooseIngredientsDialog.dishes"
-        @add="onClickAdd"
-    />
+    <ChooseIngredientsDialog v-model:model-value="chooseIngredientsDialog.show" :dishes="chooseIngredientsDialog.dishes"
+      @add="onClickAdd" />
 
     <EditDishDialog v-model="editDishDialog.show" :dish="editDishDialog.dish" @save="onClickEditDishSaveDish" />
   </q-page>
@@ -91,38 +96,38 @@ const store = useStore();
 const dishes = ref([]);
 
 const onClickIncrementDish = function (dish) {
-    dish.amount = (dish.amount == 0) ? dish.dish.standardAmount : dish.amount + 1;
-    disableChooseIngredients.value = false;
+  dish.amount = (dish.amount == 0) ? dish.dish.standardAmount : dish.amount + 1;
+  disableChooseIngredients.value = false;
 }
 
 const onClickDecrementDish = function (dish) {
-    dish.amount -= 1;
+  dish.amount -= 1;
 
-    disableChooseIngredients.value = _.sum(dishes.value.map(dish => dish.amount)) == 0;
+  disableChooseIngredients.value = _.sum(dishes.value.map(dish => dish.amount)) == 0;
 }
 
 const onClickChooseIngredients = () => {
-    chooseIngredientsDialog.value.dishes = _.cloneDeep(dishes.value.filter(dish => dish.amount > 0));
+  chooseIngredientsDialog.value.dishes = _.cloneDeep(dishes.value.filter(dish => dish.amount > 0));
 
-    chooseIngredientsDialog.value.dishes.forEach(dish => {
-        dish.dish.ingredients.forEach(ingredient => {
-            ingredient.amount.value = ingredient.amount.unit != null ? ingredient.amount.value * dish.amount : ingredient.amount.value;
-        });
-
-        dish.dish.subdishes?.forEach(subdish => {
-            subdish.dish = dishes.value.find(dish => dish.dish.id == subdish.dish).dish;
-            subdish.dish.ingredients.forEach(ingredient => {
-                dish.dish.ingredients.push({ name: ingredient.name, amount: { value: ingredient.amount.value != null ? ingredient.amount.value * dish.amount * subdish.amountFactor : null, unit: ingredient.amount.unit }, optional: ingredient.optional || subdish.optional, subdish: subdish.dish.title });
-            });
-        });
+  chooseIngredientsDialog.value.dishes.forEach(dish => {
+    dish.dish.ingredients.forEach(ingredient => {
+      ingredient.amount.value = ingredient.amount.unit != null ? ingredient.amount.value * dish.amount : ingredient.amount.value;
     });
 
-    chooseIngredientsDialog.value.show = true;
+    dish.dish.subdishes?.forEach(subdish => {
+      subdish.dish = dishes.value.find(dish => dish.dish.id == subdish.dish).dish;
+      subdish.dish.ingredients.forEach(ingredient => {
+        dish.dish.ingredients.push({ name: ingredient.name, amount: { value: ingredient.amount.value != null ? ingredient.amount.value * dish.amount * subdish.amountFactor : null, unit: ingredient.amount.unit }, optional: ingredient.optional || subdish.optional, subdish: subdish.dish.title });
+      });
+    });
+  });
+
+  chooseIngredientsDialog.value.show = true;
 };
 
 const onClickAdd = async (ingredients) => {
   if (await store.addToList(ingredients)) {
-      $notify(`Ingredients added to list!`, { actions: [{ label: 'Show', color: 'white', handler: () => { $router.push(`/list`); } }] });
+    $notify(`Ingredients added to list!`, { actions: [{ label: 'Show', color: 'white', handler: () => { $router.push(`/list`); } }] });
   }
 
   dishes.value.forEach(dish => dish.amount = 0);
@@ -136,7 +141,7 @@ const onClickEditDish = (dish) => {
 
 const onClickEditDishSaveDish = async (dish) => {
   if (await store.editDish(dish)) {
-      $notify(`${dish.title} edited!`);
+    $notify(`${dish.title} edited!`);
   }
 };
 
@@ -156,12 +161,12 @@ const ingredientToString = (ingredient) => {
     result += ingredient.name;
   }
   else {
-      if (ingredient.amount.unit == '') {
-        result += `${ingredient.amount.value} ${ingredient.name}`;
-      }
-      else {
-        result += `${ingredient.amount.value} ${ingredient.amount.unit} ${ingredient.name}`;
-      }
+    if (ingredient.amount.unit == '') {
+      result += `${ingredient.amount.value} ${ingredient.name}`;
+    }
+    else {
+      result += `${ingredient.amount.value} ${ingredient.amount.unit} ${ingredient.name}`;
+    }
   }
 
   return result;
